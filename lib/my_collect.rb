d47f75d4eb61fad7968d84d5cdd166cd70009bf5
elijahsamuels <elijahsamuels@gmail.com>
2020-11-12 21:42:38 -0700

    commit tree 763f51515808f9ca68dca35a4b990a7590292441

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,32 @@
 
+def my_collect(array)
+    i = 0
+    collection = []
+        while i < array.size
+            collection << yield(array[i])
+        i += 1
+        end
+        collection
+    end
+
+
+
+# def my_collect(languages)
+#     i = 0
+
+#     while i < languages.size 
+#         yield(languages[i])
+#         yield(students[i])
+
+#         i += 1
+#     end
+
+
+#     while i < students.size 
+#         i += 1
+#     end
+#     languages.map(&:upcase)
+#     students.map{|x| x.split(" ").first}
+
+# end
 
